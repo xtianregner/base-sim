0719707c99822566386b28309e95bc7ae9c20e9b
Christian <user@192.168.68.114>
2026-03-09 21:28:23 -0500

    Fix: use PORT variable in Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh start.sh
+web: sh -c 'cd src && gunicorn myproject.wsgi --bind 0.0.0.0:$PORT --workers 2'
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd src
-exec gunicorn myproject.wsgi --bind "0.0.0.0:${PORT}" --workers 2 --timeout 120
+exec gunicorn myproject.wsgi --bind 0.0.0.0:8000 --workers 2 --timeout 120
