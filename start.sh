c02128527d1065db40cb35dddd82a0eb5e09b71b
Christian <user@192.168.68.114>
2026-03-09 21:22:59 -0500

    Fix: use sh and quoted

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 cd src
-exec gunicorn myproject.wsgi --bind 0.0.0.0:$PORT --workers 2 --timeout 120
+exec gunicorn myproject.wsgi --bind "0.0.0.0:${PORT}" --workers 2 --timeout 120
